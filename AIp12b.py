4e90164bc8e19f72830097de7dbe2d174f5ff395
jsheh06 <jsheh06@gmail.com>
2019-10-25 15:00:41 +0800

    aip12x_aip12b5

--- AIp12b.py
+++ AIp12b.py
@@ -101,7 +101,7 @@ app = Flask(__name__)
 def index():
     global openF
     # return "index_test OK!" 
-    return "<html><body><div class='saying'>  {{ sayF }}  </div> <div class='querying'></body></html>"
+    return "<html><body>" + openF +  "</body></html>"
     # return render_template('conversationDD.html', sayF=Markup(openF))
 
 ###=== (3.4) 會話互動(interaction: Questioning-Answering)) ===###
