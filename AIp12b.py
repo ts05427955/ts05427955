e0959e826538115ddf255a411682f2cf2e69d4a6
jsheh06 <jsheh06@gmail.com>
2019-10-25 15:45:50 +0800

    aip12x_aip12b5

--- AIp12b.py
+++ AIp12b.py
@@ -101,7 +101,8 @@ app = Flask(__name__)
 def index():
     global openF
     # return "index_test OK!" 
-    return "<html><body>" + openF + "</body></html>"
+    return "<html><body>" + openF + 
+    "</body></html>"
 #    "<form method='get' action=" + url_for('interact') + ">" +
 #    "<input type='text' name='dd' size='6' background-color='888888'/>" + 
 #    "<button type='submit'>送出</button>" +
