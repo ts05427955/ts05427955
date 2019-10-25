5f477888cdd5fd1cbec9a7abdd4fd14d23a0274b
jsheh06 <jsheh06@gmail.com>
2019-10-25 22:14:59 +0800

    revised_aip12x6

--- AIp12a.py
+++ AIp12a.py
@@ -82,15 +82,20 @@
 # 建立本程式碼 (4) AIp12a.py:  (revised from AIp11c.py)
 
 from flask import Flask
+import sys
 
 app = Flask(__name__)    # __name__ 代表目前執行的模組
 
 @app.route("/")      #== 函式的裝飾(Decorator): 以函式為基礎，提供附加的功能
 def home():
+    print("This is route_home")
+    sys.stdout.flush()
     return "Hello Flask 2"
 
 @app.route("/test")  #== 代表我們要處理的網站路徑
 def test():
+    print("This is test")
+    sys.stdout.flush()
     return "This is a Test"
 
 if __name__=="__main__":  # 如果以上程式執行
