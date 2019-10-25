e3383a3120439a448cf104a16038a63d1f72da78
jsheh06 <jsheh06@gmail.com>
2019-10-25 15:04:32 +0800

    aip12x_aip12b5

--- AIp12b.py
+++ AIp12b.py
@@ -101,7 +101,10 @@ app = Flask(__name__)
 def index():
     global openF
     # return "index_test OK!" 
-    return "<html><body>" + openF +  "</body></html>"
+    return "<html><body>" + openF +  
+    "<form method='get' action=" + url_for('interact') + ">" +
+    "<input type='text' name='dd' size='6' background-color='888888'/>" + 
+    "<button type='submit'>送出</button> </form> </body></html>"
     # return render_template('conversationDD.html', sayF=Markup(openF))
 
 ###=== (3.4) 會話互動(interaction: Questioning-Answering)) ===###
