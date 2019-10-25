8bff43d6a8f6279db5d16e1851e36e79b6f4e36c
jsheh06 <jsheh06@gmail.com>
2019-10-25 22:29:44 +0800

    revised_aip12x6

--- AIp12a.py
+++ AIp12a.py
@@ -88,20 +88,20 @@ app = Flask(__name__)    # __name__ 代表目前執行的模組
 
 @app.route("/")      #== 函式的裝飾(Decorator): 以函式為基礎，提供附加的功能
 def home():
-    print("This is route_home", file=open('file.txt', 'w'))
+    print("This is route_home")
     #sys.stdout.flush()
     return "Hello Flask 2"
 
 @app.route("/test")  #== 代表我們要處理的網站路徑
 def test():
-    print("This is test", file=open('file.txt', 'w'))
+    print("This is test")
     #sys.stdout.flush()
     return "This is a Test"
 
 if __name__=="__main__":  # 如果以上程式執行
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    #gunicorn_logger = logging.getLogger('gunicorn.error')
+    #app.logger.handlers = gunicorn_logger.handlers
+    #app.logger.setLevel(gunicorn_logger.level)
     app.run()   # 立刻啟動伺服器
 
 ###=== (3.3) 將程式部署到Heroku App,並測試 ===###  
