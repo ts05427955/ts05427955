6a271edd739bc118b35df5aa21bae5f8200eff32
jsheh06 <jsheh06@gmail.com>
2019-10-26 14:50:20 +0800

    bot1

--- AIp12c.py
+++ AIp12c.py
@@ -23,9 +23,10 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    print(">>>>>>>>> testing-body:",body)
     # handle webhook body
     try:
-        print(">>>>>>>>> testing-1...")
+        print(">>>>>>>>> testing-try:...")
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
